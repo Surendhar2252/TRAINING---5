057e2142715513f3b6b9a3bb2a4de150eee89976
Surendhar M S <139234490+Surendhar2252@users.noreply.github.com>
2024-07-22 12:48:40 +0530

    Create Decode String.py

---
+++ Decode String.py
@@ -0,0 +1,15 @@
+def decodeString(s):
+    stack = []
+    for char in s:
+        if char != ']':
+            stack.append(char)
+        else:
+            substr = ''
+            while stack and stack[-1] != '[':
+                substr = stack.pop() + substr
+            stack.pop()
+            k = ''
+            while stack and stack[-1].isdigit():
+                k = stack.pop() + k
+            stack.append(int(k) * substr)
+    return ''.join(stack)
